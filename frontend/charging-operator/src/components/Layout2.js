37d8a25535e13268897769ab63321d4e65c6ba0b
Markos Baratsas <el17010@mail.ntua.gr>
2022-04-03 16:07:00 +0300

    Remove footer from Layout2

--- frontend/charging-operator/src/components/Layout2.js
+++ frontend/charging-operator/src/components/Layout2.js
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import Navbar2 from '../components/Navbar2';
-import Footer from '../components/Footer';
 
 const Layout2 = () => {
     return (
@@ -9,7 +8,6 @@ const Layout2 = () => {
             <div className="content">
                 <Outlet />
             </div>
-            <Footer />
         </>
     );
 }
