ada6e65b625b4584d751bfd2d3250606da58812c
Markos Baratsas <el17010@mail.ntua.gr>
2022-04-25 19:17:51 +0300

    Update Dashboard with loading styles

--- frontend/charging-operator/src/pages/app/Dashboard.js
+++ frontend/charging-operator/src/pages/app/Dashboard.js
@@ -22,22 +22,26 @@ const Dashboard = () => {
             <h1 className="app-page-title">Charging Stations Dashboard</h1>
             <div className="stations-list flex-row-start-start">
                 {!stations
-                ? <ReactLoading type="spin" color="#202020" height={80} width={80} className="loading"/>
-                : stations.map(station => (
-                <Link className="station-preview" to={`/app/station-${station.id}`} key={station.id}>
-                    <h2>{ upTo(station.name, 20) }</h2>
-                    <p className="address">{ upTo(station.address, 20) }</p>
-                    <p className="availability"><span>{ station.occupied_chargers }/{ station.all_chargers }</span> chargers occupied</p>
-                </Link>
-                ))}
-                <Link className="add-station flex-column-center-center" to="/app/new-station">
-                    <h2>+</h2>
-                    <p>Create a New Charging Station</p>
-                </Link>
-                <Link className="add-station flex-column-center-center" to="/app/add-station">
-                    <h2>+</h2>
-                    <p>Add an existing Charging Station</p>
-                </Link>
+                ? <ReactLoading type="spin" color="#f3f3f3" height={80} width={80} className="loading-center"/>
+                : (
+                <>
+                    {stations.map(station => (
+                        <Link className="station-preview" to={`/app/station-${station.id}`} key={station.id}>
+                            <h2>{ upTo(station.name, 20) }</h2>
+                            <p className="address">{ upTo(station.address, 20) }</p>
+                            <p className="availability"><span>{ station.occupied_chargers }/{ station.all_chargers }</span> chargers occupied</p>
+                        </Link>
+                    ))}
+                    <Link className="add-station flex-column-center-center" to="/app/new-station">
+                        <h2>+</h2>
+                        <p>Create a New Charging Station</p>
+                    </Link>
+                    <Link className="add-station flex-column-center-center" to="/app/add-station">
+                        <h2>+</h2>
+                        <p>Add an existing Charging Station</p>
+                    </Link>
+                </>
+                )}
             </div>
         </section>
     );
