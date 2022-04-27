f2aeec029f5f9f62a951d80f3ca13778541b066b
Markos Baratsas <el17010@mail.ntua.gr>
2022-04-27 14:45:30 +0300

    Remove MethodConstantPricingGroup and add MethodConstantStation
    It makes more sense to add a whole station rather than just a
    Pricing Group, because other stations' operators should not be
    able to view another station's PricingGroups

--- backend/chargers/models.py
+++ backend/chargers/models.py
@@ -56,13 +56,13 @@ class MethodConstantBool(models.Model):
 {str(self.value)}'
 
 
-class MethodConstantPricingGroup(models.Model):
+class MethodConstantStation(models.Model):
     id = models.AutoField(primary_key=True)
     pricing_group = models.ForeignKey(PricingGroup, on_delete=models.CASCADE,
                                       related_name='pricing_group')
     name = models.CharField(max_length=31, default='')
-    value = models.ForeignKey(PricingGroup, on_delete=models.CASCADE,
-                              related_name='value')
+    value = models.ForeignKey(Station, on_delete=models.SET_NULL,
+                              related_name='value', null=True)
 
     def __str__(self):
         return f'{self.id}, {self.pricing_group}, {self.name},\
@@ -79,7 +79,7 @@ class Charger(models.Model):
     id = models.AutoField(primary_key=True)
     pricing_group = models.ForeignKey(PricingGroup, on_delete=models.CASCADE)
     name = models.CharField(max_length=31, default='')
-    type = models.CharField(max_length=63, default='')
+    connector_type = models.CharField(max_length=63, default='')
     current = models.CharField(max_length=63, default='',
                                     choices=Current.choices)
     power = models.DecimalField(max_digits=8, decimal_places=4, default=0)
