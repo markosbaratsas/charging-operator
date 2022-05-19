d2ed003381e30dd03169de074d75f53725e7710a
Markos Baratsas <el17010@mail.ntua.gr>
2022-05-19 11:39:56 +0300

    Add file that was not saved

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,7 +6,7 @@ import users.views as views
 urlpatterns = [
     path('register', views.register_operator, name="register_operator"),
     path('login', obtain_auth_token, name="obtain_auth_token"),
-    path('logout', views.delete_token_operator, name="delete_token_operator"),
+    path('logout', views.delete_token, name="delete_token"),
     path('validate-token', views.validate_token_operator,
                     name="validate_token_operator"),
     path('validate-token-owner', views.validate_token_owner,
