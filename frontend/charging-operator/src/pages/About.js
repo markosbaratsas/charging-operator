e8601dede5c72c91b17d653b4c940ee6550fcfbf
Markos Baratsas <el17010@mail.ntua.gr>
2022-06-02 17:05:36 +0300

    Update About and Contact pages to look good on phones too

--- frontend/charging-operator/src/pages/About.js
+++ frontend/charging-operator/src/pages/About.js
@@ -1,14 +1,17 @@
 import useTitle from '../hooks/useTitle';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 
 const About = ({title}) => {
     useTitle({title});
+    const { width } = useWindowDimensions();
 
+    if (width >= 1000) {
     return (
         <main className="flex-column-center-center">
             <div className="about-page">
                 <div className="about-words">
-                    <h1>About Charging operator</h1>
+                    <h1>About Charging Operator</h1>
                     <p>
                         Charging operator is an app used by EV Charging Station <br />
                         operators, so that they can easily operate their stations. <br />
@@ -23,6 +26,31 @@ const About = ({title}) => {
             </div>
         </main>
     );
+    }
+    else {
+        return (
+            <main className="flex-column-center-center">
+                <div className="about-page-phone">
+                    <div className="about-image-phone"></div>
+                    <div className="about-words-phone">
+                        <h1>About Charging Operator</h1>
+                        <p>
+                            Charging operator is an app used by EV Charging Station
+                            operators, so that they can easily operate their stations.
+                            Despite the fact that it is built to make operators' lives
+                            easier, it supports some functionality for EV owners, so that
+                            they can reserve their vehicle's charging beforehand.
+                            <br />
+                            <br />
+                            It was built in 2022 as part of
+                            <br />
+                            <a target="_blank" href="https://www.linkedin.com/in/markos-baratsas/">Markos Baratsas</a>' Master Thesis.
+                        </p>
+                    </div>
+                </div>
+            </main>
+        );
+    }
 }
  
 export default About;
--- frontend/charging-operator/src/pages/Contact.js
+++ frontend/charging-operator/src/pages/Contact.js
@@ -3,6 +3,7 @@ import ReactLoading from 'react-loading';
 
 import { contactForm } from '../api/BackendCalls';
 import useTitle from '../hooks/useTitle';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 
 const FULLNAME_REGEX = /^[0-9_\ A-Za-zΑ-Ωα-ωίϊΐόάέύϋΰήώ]{2,40}$/;
@@ -11,6 +12,7 @@ const MESSAGE_REGEX = /^[0-9_\ A-Za-zΑ-Ωα-ωίϊΐόάέύϋΰήώ]{2,400}$/;
 
 const Contact = ({title}) => {
     useTitle({title});
+    const { width } = useWindowDimensions();
 
     const [fullname, setFullname] = useState("");
     const [fullnameError, setFullnameError] = useState(false);
@@ -56,6 +58,7 @@ const Contact = ({title}) => {
         setMessageError(false);
     }, [fullname, email, message])
 
+    if (width >= 1000) {
     return (
         <main className="flex-column-center-center">
             <div className="contact-page">
@@ -113,6 +116,67 @@ const Contact = ({title}) => {
             </div>
         </main>
     );
+    }
+    else {
+        return (
+            <main className="flex-column-center-center">
+                <div className="contact-page-phone">
+                    <div className="contact-image-phone"></div>
+                    <div className="flex-column-center-center contact-words-phone">
+                        <h1>Charging Operator</h1>
+                        <p>Contact us using the form below. </p>
+
+                        <hr className="big-hr"/>
+
+                        <div className="flex-column-center-center contact-f-phone">
+
+                            <div className="flex-row-center-center">
+                                <input
+                                    type="text"
+                                    className={"my-classic-input contact-input1-phone" + " " + (fullnameError ? "error-selected" : "")}
+                                    placeholder="Full Name"
+                                    value={fullname}
+                                    onChange={(e) => setFullname(e.target.value)}
+                                />
+                                <input
+                                    type="email"
+                                    className={"my-classic-input contact-input1-phone" + " " + (emailError ? "error-selected" : "")}
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+
+                            <textarea
+                                className={"my-classic-input contact-input2-phone" + " " + (messageError ? "error-selected" : "")}
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                rows="4"
+                                cols="50"
+                                placeholder="Message"
+                            />
+                            <br />
+
+                            {(fullnameError || emailError || messageError) ?
+                                <p className="error-p-input">
+                                    Please provide a fullname shorter than 40 chars, a valid email, a message with <br />
+                                    no more than 400 characters and resubmit.
+                                </p>
+                            : null}
+
+                            {!clicked ?
+                                <button className="submit-charger" onClick={handleSubmit}>
+                                    Send!
+                                </button>
+                            : !success ? <ReactLoading type="spin" color="#202020" height={50} width={50}/>
+                            : <p className="green-p">Successfully sent. Thank you for contacting us!</p>}
+                        </div>
+                    </div>
+                </div>
+            </main>
+        );
+    }
+
 }
  
 export default Contact;
